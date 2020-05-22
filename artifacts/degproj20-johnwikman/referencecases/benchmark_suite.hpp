fb5bed57ca96183a8ab9d4652fd25f9445534959
John Wikman <john.wikman@hotmail.com>
2020-05-22 15:03:47 +0200

    Test new benchmark setup.

--- artifacts/degproj20-johnwikman/referencecases/benchmark_suite.hpp
+++ artifacts/degproj20-johnwikman/referencecases/benchmark_suite.hpp
@@ -29,19 +29,27 @@
             fn_post();                                      \
         } while (0)
 
-#define BENCHMARK_PRINTRESULTS(name, a)                                                \
-        do {                                                                           \
-            std::cout << "[" << name << "_results]" << std::endl;                  \
-            std::sort(std::begin(a), std::end(a));                                     \
-            double median = a[a.size() / 2];                                           \
-            double avg = a.sum() / ((double) a.size());                                \
-            double variance = std::max(a.max() - avg, avg - a.min());                  \
-            std::cout << "no_of_iterations = " << a.size() << std::endl;               \
-            std::cout << "median_ms = " << (median * 1000.0) << std::endl;        \
+#define BENCHMARK_PRINTRESULTS(name, a)                                           \
+        do {                                                                      \
+            int i;                                                                \
+            std::cout << "[" << name << "_results]" << std::endl;                 \
+            std::sort(std::begin(a), std::end(a));                                \
+            double median = a[a.size() / 2] * 1000.0;                             \
+            double avg = (a.sum() / ((double) a.size())) * 1000.0;                \
+            double variance = (avg - (a[0] * 1000.0)) * (avg - (a[0] * 1000.0));  \
+            std::cout << "no_of_iterations = " << a.size() << std::endl;          \
+            std::cout << "median_ms = " << (median) << std::endl;                 \
             std::cout << "longest_run_ms = " << (a.max() * 1000.0) << std::endl;  \
             std::cout << "shortest_run_ms = " << (a.min() * 1000.0) << std::endl; \
-            std::cout << "average_ms = " << (avg * 1000.0) << std::endl;          \
-            std::cout << "variance_ms = " << (variance * 1000.0) << std::endl;  \
+            std::cout << "average_ms = " << (avg) << std::endl;                   \
+            std::cout << "points = [" << (a[0] * 1000.0);                         \
+            for (i = 1; i < (int) a.size(); ++i) {                                \
+                std::cout << ", " << (a[i]);                                      \
+                variance += (avg - (a[i] * 1000.0)) * (avg - (a[i] * 1000.0))     \
+            }                                                                     \
+            std::cout << "]" << std::endl;                                        \
+            variance /= (double) (a.size() - 1)                                   \
+            std::cout << "variance_ms = " << variance << std::endl;               \
         } while (0)
 
 #define BENCHMARK(fn_prep, fn_run, fn_post)                                     \
